88ccde47582b3242e04b20eedcc99d5f95992995
Vladyslav Harshyn <garshyn_98@ukr.net>
2022-08-30 19:21:04 +0300

    rewrite everything on hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Container } from './App.styled';
 import { Searchbar } from './components/Searchbar/Searchbar';
 import { ImageGallery } from './components/ImageGallery/ImageGallery';
@@ -7,114 +7,184 @@ import { Button } from './shared/Button/Button';
 import ModalWindow from './shared/Modal/Modal';
 import * as API from './components/services/pixabay';
 
-export class App extends Component {
-  state = {
-    page: 1,
-    query: '',
-    items: [],
-    currentLargeImageURL: '',
-    error: null,
-    isLoading: false,
-  };
+export const App = () => {
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [items, setItems] = useState([]);
+  const [currentLargeImageURL, setCurrentLargeImageURL] = useState('');
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  onOpenModalWithLargeImage = url => {
-    this.setState({
-      currentLargeImageURL: url,
-    });
-  };
+  useEffect(() => {
+    if (query !== '') {
+      addImages(query, page);
+    }
+  }, [query, page]);
 
-  onModalClose = () => {
-    this.setState({
-      currentLargeImageURL: '',
-    });
-  };
-
-  onFormSubmit = query => {
-    if (query.trim().length === 0) {
+  const onFormSubmit = newQuery => {
+    if (newQuery.trim().length === 0) {
       alert('Please, enter request');
       return;
     }
 
-    this.setState({
-      query,
-      page: 1,
-      items: [],
-    });
+    setQuery(newQuery);
+    setPage(1);
+    setItems([]);
   };
 
-  onLoadMoreButton = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  addImages = async (query, page) => {
+  const addImages = async (query, page) => {
     try {
-      this.setState({
-        isLoading: true,
-      });
-      const images = await API.loadImage(query, page);
+      setIsLoading(true);
+      const image = await API.loadImage(query, page);
+      setItems(prevState => [...prevState, ...image]);
+      setIsLoading(false);
 
-      this.setState(prevState => ({
-        items: [...prevState.items, ...images],
-        isLoading: false,
-      }));
-      if (images.length === 0) {
+      if (image.length === 0) {
         alert(
           "Sorry, we can't find anyting for your request. Please, enter another request"
         );
       }
     } catch (error) {
-      this.setState({
-        error: error.message,
-      });
+      setError(error.message);
     } finally {
-      this.setState({
-        isLoading: false,
-      });
+      setIsLoading(false);
     }
   };
 
-  componentDidUpdate(_, prevState) {
-    if (
-      prevState.page !== this.state.page ||
-      prevState.query !== this.state.query
-    ) {
-      this.addImages(this.state.query, this.state.page);
-    }
-  }
+  return (
+    <Container>
+      <Searchbar onSubmit={onFormSubmit} isLoading={isLoading} />
+      {error && <p>{error}</p>}
+      {items.length > 0 && (
+        <ImageGallery items={items} onClick={setCurrentLargeImageURL} />
+      )}
+      {isLoading && (
+        <ThreeDots height="50" width="50" color="#303f9f" ariaLabel="loading" />
+      )}
+      {items.length > 0 && (
+        <Button
+          onLoadMore={() => setPage(prev => prev + 1)}
+          isLoading={isLoading}
+        />
+      )}
+      {currentLargeImageURL && (
+        <ModalWindow
+          src={currentLargeImageURL}
+          closeModal={() => setCurrentLargeImageURL('')}
+        />
+      )}
+    </Container>
+  );
+};
 
-  render() {
-    const { items, currentLargeImageURL, isLoading, error } = this.state;
+// export class App extends Component {
+//   state = {
+//     page: 1,
+//     query: '',
+//     items: [],
+//     currentLargeImageURL: '',
+//     error: null,
+//     isLoading: false,
+//   };
 
-    return (
-      <Container>
-        <Searchbar onSubmit={this.onFormSubmit} isLoading={isLoading} />
-        {error && <p>{error}</p>}
-        {items.length > 0 && (
-          <ImageGallery
-            items={items}
-            onClick={this.onOpenModalWithLargeImage}
-          />
-        )}
-        {isLoading && (
-          <ThreeDots
-            height="50"
-            width="50"
-            color="#303f9f"
-            ariaLabel="loading"
-          />
-        )}
-        {items.length > 0 && (
-          <Button onLoadMore={this.onLoadMoreButton} isLoading={isLoading} />
-        )}
-        {currentLargeImageURL && (
-          <ModalWindow
-            src={currentLargeImageURL}
-            closeModal={this.onModalClose}
-          />
-        )}
-      </Container>
-    );
-  }
-}
+//   onOpenModalWithLargeImage = url => {
+//     this.setState({
+//       currentLargeImageURL: url,
+//     });
+//   };
+
+//   onModalClose = () => {
+//     this.setState({
+//       currentLargeImageURL: '',
+//     });
+//   };
+
+//   onFormSubmit = query => {
+//     if (query.trim().length === 0) {
+//       alert('Please, enter request');
+//       return;
+//     }
+
+//     this.setState({
+//       query,
+//       page: 1,
+//       items: [],
+//     });
+//   };
+
+//   onLoadMoreButton = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   addImages = async (query, page) => {
+//     try {
+//       this.setState({
+//         isLoading: true,
+//       });
+//       const images = await API.loadImage(query, page);
+
+//       this.setState(prevState => ({
+//         items: [...prevState.items, ...images],
+//         isLoading: false,
+//       }));
+//       if (images.length === 0) {
+//         alert(
+//           "Sorry, we can't find anyting for your request. Please, enter another request"
+//         );
+//       }
+//     } catch (error) {
+//       this.setState({
+//         error: error.message,
+//       });
+//     } finally {
+//       this.setState({
+//         isLoading: false,
+//       });
+//     }
+//   };
+
+//   componentDidUpdate(_, prevState) {
+//     if (
+//       prevState.page !== this.state.page ||
+//       prevState.query !== this.state.query
+//     ) {
+//       this.addImages(this.state.query, this.state.page);
+//     }
+//   }
+
+//   render() {
+//     const { items, currentLargeImageURL, isLoading, error } = this.state;
+
+//     return (
+//       <Container>
+//         <Searchbar onSubmit={this.onFormSubmit} isLoading={isLoading} />
+//         {error && <p>{error}</p>}
+//         {items.length > 0 && (
+//           <ImageGallery
+//             items={items}
+//             onClick={this.onOpenModalWithLargeImage}
+//           />
+//         )}
+//         {isLoading && (
+//           <ThreeDots
+//             height="50"
+//             width="50"
+//             color="#303f9f"
+//             ariaLabel="loading"
+//           />
+//         )}
+//         {items.length > 0 && (
+//           <Button onLoadMore={this.onLoadMoreButton} isLoading={isLoading} />
+//         )}
+//         {currentLargeImageURL && (
+//           <ModalWindow
+//             src={currentLargeImageURL}
+//             closeModal={this.onModalClose}
+//           />
+//         )}
+//       </Container>
+//     );
+//   }
+// }
